fd811d126a4e0798b7f9b5a2093a9f76f4cca6bc
Wergon74 <satorugodze17@gmail.com>
2026-05-27 18:40:26 +0700

    Добавил новые классы

---
+++ Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TaskFlow {
+  public class Task {
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public void Complete() {
+      IsCompleted = true;
+    }
+  }
+}
